a0071215931712d7d77cac8d79b7dc1bba1b6190
Kerinlin <18296613547@qq.com>
2018-05-14 23:13:20 +0800

    模块化 小程序 界面小技巧

---
+++ wxapp/sj/modules/test-drive.js
@@ -0,0 +1,13 @@
+// es6模块化语法 module
+// js变量或常量值类型由值决定
+const testDrive= ()=>{
+    /* 用内置API 原生的API 大部分的小程序是html5 webview  性能比原生的慢一些
+    
+    
+    
+    */
+    wx.showToast({
+        title:'暂不支持'
+    })
+}
+export default testDrive
