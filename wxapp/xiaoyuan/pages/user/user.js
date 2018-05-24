e0f53e902806ab114aa1552c81693ff1756bbfdd
Kerinlin <18296613547@qq.com>
2018-05-24 21:26:52 +0800

    小程序个人详情页练习

---
+++ wxapp/xiaoyuan/pages/user/user.js
@@ -0,0 +1,30 @@
+// pages/user/user.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    userInfo: {
+      avatar: 'https://wx.qlogo.cn/mmopen/vi_32/Q0j4TwGTfTJDhKQ0zkRDOZOhGjWlTOkb6odRZkPmegyVVBicaicibkCqQADd1MyoibejVRwmcAXm7OGM3wY8XPicFtA/132',
+      nickName: '喻顺武',
+      isBindEdu: true
+    },
+    stu_id: '0414'
+  },
+  
+
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
